97eb3b7c3bae1577b2d57f776648980803db8808
gthetwong <gthetwong@gmail.com>
2014-01-20 18:38:45 -0800

    Category pages work, but are not linked

--- app.rb
+++ app.rb
@@ -1,19 +1,20 @@
-require 'pry'
+require 'pry' 
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 
 def dbname
-  "storeadminsite"
+  "lab11"
 end
 
 def with_db
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
   yield c
   c.close
 end
 
 get '/' do
+
   erb :index
 end
 
@@ -21,7 +22,7 @@ end
 
 # Get the index of products
 get '/products' do
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
 
   # Get all rows from the products table.
   @products = c.exec_params("SELECT * FROM products;")
@@ -36,9 +37,10 @@ end
 
 # POST to create a new product
 post '/products' do
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
 
-  # Insert the new row into the products table.
+  # Insert the new row into the products ta
+ble.
   c.exec_params("INSERT INTO products (name, price, description) VALUES ($1,$2,$3)",
                   [params["name"], params["price"], params["description"]])
 
@@ -51,7 +53,7 @@ end
 
 # Update a product
 post '/products/:id' do
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
 
   # Update the product.
   c.exec_params("UPDATE products SET (name, price, description) = ($2, $3, $4) WHERE products.id = $1 ",
@@ -61,7 +63,7 @@ post '/products/:id' do
 end
 
 get '/products/:id/edit' do
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
   @product = c.exec_params("SELECT * FROM products WHERE products.id = $1", [params["id"]]).first
   c.close
   erb :edit_product
@@ -69,7 +71,7 @@ end
 # DELETE to delete a product
 post '/products/:id/destroy' do
 
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
   c.exec_params("DELETE FROM products WHERE products.id = $1", [params["id"]])
   c.close
   redirect '/products'
@@ -77,46 +79,78 @@ end
 
 # GET the show page for a particular product
 get '/products/:id' do
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
   @product = c.exec_params("SELECT * FROM products WHERE products.id = $1;", [params[:id]]).first
   c.close
   erb :product
 end
 
-def create_products_table
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
-  c.exec %q{
-  CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name varchar(255),
-    price decimal,
-    description text
-  );
-  }
+######################################
+
+# The Categories machinery:
+
+# Get the index of categories
+get '/categories' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
+
+  # Get all rows from the categories table.
+  @categories = c.exec_params("SELECT * FROM categories;")
   c.close
+  erb :categories
 end
 
-def drop_products_table
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
-  c.exec "DROP TABLE products;"
-  c.close
+# Get the form for creating a new product
+get '/categories/new' do
+  erb :new_category
 end
 
-def seed_products_table
-  products = [["Laser", "325", "Good for lasering."],
-              ["Shoe", "23.4", "Just the left one."],
-              ["Wicker Monkey", "78.99", "It has a little wicker monkey baby."],
-              ["Whiteboard", "125", "Can be written on."],
-              ["Chalkboard", "100", "Can be written on.  Smells like education."],
-              ["Podium", "70", "All the pieces swivel separately."],
-              ["Bike", "150", "Good for biking from place to place."],
-              ["Kettle", "39.99", "Good for boiling."],
-              ["Toaster", "20.00", "Toasts your enemies!"],
-             ]
+# POST to create a new category
+post '/categories' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
 
-  c = PGconn.new(:host => "localhost", :dbname => dbname)
-  products.each do |p|
-    c.exec_params("INSERT INTO products (name, price, description) VALUES ($1, $2, $3);", p)
-  end
+  # Insert the new row into the categories table.
+  c.exec_params("INSERT INTO categories (name, description) VALUES ($1,$2)",
+                  [params["name"], params["description"]])
+
+  # Assuming you created your categories table with "id SERIAL PRIMARY KEY",
+  # This will get the id of the category you just created.
+  new_category_id = c.exec_params("SELECT currval('categories_id_seq');").first["currval"]
   c.close
+  redirect "/categories/#{new_category_id}"
+end
+
+# Update a category
+post '/categories/:id' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
+
+  # Update the category.
+  c.exec_params("UPDATE categories SET (name, description) = ($2, $3) WHERE categories.id = $1 ",
+                [params["id"], params["name"], params["description"]])
+  c.close
+  redirect "/categories/#{params["id"]}"
+end
+
+get '/categories/:id/edit' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
+  @category = c.exec_params("SELECT * FROM categories WHERE categories.id = $1", [params["id"]]).first
+  @cat_products =c.exec_params("SELECT * FROM products_categories WHERE categories.id=$1")
+
+  c.close
+  erb :edit_category
+end
+# DELETE to delete a category
+post '/categories/:id/destroy' do
+
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
+  c.exec_params("DELETE FROM categories WHERE categories.id = $1", [params["id"]])
+  c.close
+  redirect '/categories'
+end
+
+# GET the show page for a particular category
+get '/categories/:id' do
+  c = PGconn.new(:host => "localhost", :dbname => dbname, :password => "xennifer")
+  @category = c.exec_params("SELECT * FROM categories WHERE categories.id = $1;", [params[:id]]).first
+  c.close
+  erb :category
 end
